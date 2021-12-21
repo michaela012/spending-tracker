d5f15382ba8c2f97ad247baaf61b22d40fdf4f29
mika <mikajs37@gmail.com>
2021-12-21 16:47:52 -0500

    budget model updated, migrations init

--- app/models/budget.py
+++ app/models/budget.py
@@ -1,20 +1,12 @@
 from app import db
+from app.models.user import User
 
 class Budget(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  email = db.Column(db.String(120), index=True, unique=True)
-  password_hash = db.Column(db.String(128))
-  name = db.Column(db.String(120))
-  total_budget = db.Column(db.Integer)
-  _phone_number = db.Column(db.Unicode(20), nullable=False)
-  phone_country_code = db.Column(db.Unicode(8))
-
-  phone_number = db.composite(
-    PhoneNumber,
-    _phone_number,
-    phone_country_code
-  )
-
-
+  name = db.Column(db.String(120), index=True)
+  allotted_amount = db.Column(db.Integer)
+  amount_remaining = db.Column(db.Integer)
+  user_id = db.Column(db.Integer, db.ForeignKey(user.id))
 
   def __repr__(self):
+    return f"{self.name}: total amount: {self.allotted_amount}, amount remaining: {self.amount_remaining}. Owned by User {self.user_id}"
